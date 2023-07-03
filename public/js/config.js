861cabe17d4ff9b858323fbfbf8e0ddca415faee
ArkhamCookie <arkhamcookie@proton.me>
2023-07-03 05:21:36 +0000

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,14 @@
+// might convert to json file
+
+const CONFIG = {
+	commandPathDelimiter: '/',
+	commandSearchDelimiter: ' ',
+	defaultSearchTemplate: 'https://duckduckgo.com/?q={}',
+	openLinksInNewTab: true,
+	rainColumns: 200,
+    rainFade: 0.2,
+    rainFrameRate: 1000 / 18,
+    rainResilience: 0.8,
+    rainSpeed: 1,
+    suggestionLimit: 4,
+}
